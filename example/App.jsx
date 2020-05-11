27cc49e3a5112ca3b872ed1a6765ccea923a0d87
Ian Lai <ian24257587@gmail.com>
2020-05-11 16:49:09 +0800

    build: project environment building

---
+++ example/App.jsx
@@ -0,0 +1,15 @@
+// @flow
+/* eslint import/no-extraneous-dependencies: 0 */
+
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+function App() {
+  return (
+    <div>
+      testingggggggg
+    </div>
+  );
+}
+
+export default hot(module)(App);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import Table from './components/Table/Table';
+// import TableField from './components/Table/TableField';
+
+// export {
+//   Table,
+//   TableField,
+// };
